e942a71ce3eed33722dea0b34a8cb405e2d96ac2
Perry Jones <perry.jones@softwire.com>
2021-11-07 16:45:58 +0000

    Added Default Tile

---
+++ lib/default_tile.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class DefaultTile extends StatefulWidget {
+  final EdgeInsets padding;
+  final Color color;
+  final String text;
+  final TextStyle style;
+  final bool isAllCornersRounded;
+
+  const DefaultTile({
+    Key? key,
+    this.padding = const EdgeInsets.fromLTRB(0, 20, 0, 20),
+    this.color = Colors.green,
+    this.text = 'Test text',
+    this.style = const TextStyle(fontSize: 30),
+    this.isAllCornersRounded = false,
+  }) : super(key: key);
+
+  @override
+  _DefaultTileState createState() => _DefaultTileState();
+}
+
+class _DefaultTileState extends State<DefaultTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: widget.padding,
+      decoration: BoxDecoration(
+        color: widget.color,
+        borderRadius: widget.isAllCornersRounded
+          ? const BorderRadius.only(
+            topLeft: Radius.circular(10),
+            bottomRight: Radius.circular(10))
+          : const BorderRadius.all(Radius.circular(10),
+        ),
+      ),
+      width: double.infinity,
+      child: Center(
+        child: Text(
+          widget.text,
+          style: widget.style
+        )
+      )
+    );
+  }
+}
