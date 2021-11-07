7f749fa8bd56a2c7479e1b776a5d2e084b3c9d83
Perry Jones <perry.jones@softwire.com>
2021-11-07 22:37:13 +0000

    Tidied Banner and New Challenge Button

--- lib/discover_banner.dart
+++ lib/discover_banner.dart
@@ -1,26 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:gunfight/default_tile.dart';
 
 class DiscoverPageBanner extends StatelessWidget {
   const DiscoverPageBanner({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
-      decoration: const BoxDecoration(
-        color: Colors.green,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(10),
-          bottomRight: Radius.circular(10),
-        ),
-      ),
-      width: double.infinity,
-      child: const Center(
-        child: Text(
-          'Banner',
-          style: TextStyle(fontSize: 30)
-        )
-      )
+    return const DefaultTile(
+      text: 'Banner',
+      color: Colors.green,
     );
   }
 }
--- lib/discover_new_challenge_button.dart
+++ lib/discover_new_challenge_button.dart
@@ -1,26 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'default_tile.dart';
+
 class DiscoverNewChallengeButton extends StatelessWidget {
   const DiscoverNewChallengeButton({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.fromLTRB(0, 40, 0, 40),
-      decoration: const BoxDecoration(
-        color: Colors.blue,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(10),
-          bottomRight: Radius.circular(10),
-        ),
-      ),
-      width: double.infinity,
-      child: const Center(
-        child: Text(
-          'New Challenge Button',
-          style: TextStyle(fontSize: 30)
-        )
-      )
+    return const DefaultTile(
+      padding: EdgeInsets.fromLTRB(0, 40, 0, 40),
+      text: 'New Challenge Button',
+      color: Colors.blue,
     );
   }
 }
