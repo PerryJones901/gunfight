cbf9d8903eb00db8669bd90a41dfb78c91938af3
Perry Jones <perry.jones@softwire.com>
2021-11-08 20:37:32 +0000

    Added Stateful Template

---
+++ lib/stateful_template.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WidgetName extends StatefulWidget {
+  const WidgetName({Key? key}) : super(key: key);
+
+  @override
+  WidgetNameState createState() => WidgetNameState();
+}
+
+class WidgetNameState extends State<WidgetName> {
+  @override
+  Widget build(BuildContext context){
+    return Container();
+  }
+}
